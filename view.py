dca2cad5146da337b7beb7053337eee7f4fd6133
Dcblah <92977649+Dcblah@users.noreply.github.com>
2023-07-26 19:48:15 +0530

    Update view.py

--- view.py
+++ view.py
@@ -11,8 +11,7 @@ def app(car_df):
 
     # Display descriptive statistics.
     st.subheader("Columns Description:")
-    if st.checkbox("Show summary"):
-        st.table(car_df.describe())
+    st.table(car_df.describe())
 
     # ADD NEW CODE FROM HERE
     # Add a subheader and create three columns. Store the columns in two separate variables.
@@ -20,16 +19,13 @@ def app(car_df):
 
     # Add a checkbox in the first column. Display the column names of 'car_df' on the click of checkbox.
     with beta_col1:
-        if st.checkbox("Show all column names"):
-            st.table(list(car_df.columns))
+        st.table(list(car_df.columns))
 
     # Add a checkbox in the second column. Display the column data-types of 'car_df' on the click of checkbox.
     with beta_col2:
-        if st.checkbox("View column data-type"):
-            st.table(car_df.dtypes)
+        st.table(car_df.dtypes)
 
     # Add a checkbox in the third column followed by a selectbox which accepts the column name whose data needs to be displayed.
     with beta_col3:
-        if st.checkbox("View column data"):
-            column_data = st.selectbox('Select column', tuple(car_df.columns))
-            st.write(car_df[column_data])
+        column_data = st.selectbox('Select column', tuple(car_df.columns))
+        st.write(car_df[column_data])
