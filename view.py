c653b7fb9200db03b99a743d674ef2aea5d0b279
Dcblah <92977649+Dcblah@users.noreply.github.com>
2023-07-26 19:50:03 +0530

    Update view.py

--- view.py
+++ view.py
@@ -7,11 +7,11 @@ def app(car_df):
     # Displaying orginal dataset
     st.header("View Data")
     # Add an expander and display the dataset as a static table within the expander.
-    st.table(car_df)
+    st.write(car_df)
 
     # Display descriptive statistics.
     st.subheader("Columns Description:")
-    st.table(car_df.describe())
+    st.write(car_df.describe())
 
     # ADD NEW CODE FROM HERE
     # Add a subheader and create three columns. Store the columns in two separate variables.
@@ -19,11 +19,11 @@ def app(car_df):
 
     # Add a checkbox in the first column. Display the column names of 'car_df' on the click of checkbox.
     with beta_col1:
-        st.table(list(car_df.columns))
+        st.write(list(car_df.columns))
 
     # Add a checkbox in the second column. Display the column data-types of 'car_df' on the click of checkbox.
     with beta_col2:
-        st.table(car_df.dtypes)
+        st.write(car_df.dtypes)
 
     # Add a checkbox in the third column followed by a selectbox which accepts the column name whose data needs to be displayed.
     with beta_col3:
